c85264aa508b173a4d32398d0723b6fee9f0490a
nitinmyna <nitinmyna@gmail.com>
2026-09-25 15:01:30 +0530

    client app code added

---
+++ day5/tickets_app/client/src/App.jsx
@@ -0,0 +1,19 @@
+function Navigation() {
+    
+    return (
+       <>
+       
+       </>
+    );
+}
+
+function App() {
+    return (
+        <>
+        My App
+       </>
+    );
+}
+
+export default App;
+
